8e1cd9e412c5d84e42f2ca8ab1d0f79c476edaf2
Cláudia Martins <claudiainescostamartins@gmail.com>
2020-05-19 09:55:31 +0100

    Peer backing up file -> low quality in images why?

--- src/com/assigment_2/SSLEngine/SSLEngineHandler.java
+++ src/com/assigment_2/SSLEngine/SSLEngineHandler.java
@@ -216,7 +216,7 @@ public abstract class SSLEngineHandler {
 
     protected void write(SocketChannel socketChannel, SSLEngine engine, byte[] message) throws Exception {
         myAppData.clear();
-        myAppData = ByteBuffer.allocateDirect(message.length);
+        myAppData = ByteBuffer.allocate(message.length);
         myAppData.put(message);
         myAppData.flip();
 
